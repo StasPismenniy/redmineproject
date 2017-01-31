25fc1bc0599843e4ab1b956238d80392dd4353a3
Stas <stas@ekreative.com>
2017-01-31 09:41:03 +0200

    return object

--- js/controllers/loginController.js
+++ js/controllers/loginController.js
@@ -12,11 +12,17 @@ app.controller('loginCtrl', function ($scope, Api, localStorageService, $state) 
         Api.login($scope.username ,$scope.pass )
             .then(function (data, status) {
             console.log(data);
+
                 localStorageService.set('username', $scope.username);
+
                 console.log('yes username');
+
                 localStorageService.set('pass', $scope.pass);
+
                 console.log('yes');
+
                 $state.go('main.projects');
+
         }, function (data, status) {
             console.log(data);
                 $scope.err = true
--- js/controllers/projectController.js
+++ js/controllers/projectController.js
@@ -3,6 +3,8 @@ app.controller('projectsCtrl', function ($scope, Api) {
         .then(function (data, status) {
             console.log(data);
 
+            $scope.projects = data.data.projects;
+            console.log($scope.projects);
         }, function (data, status) {
             console.log(data);
         });
