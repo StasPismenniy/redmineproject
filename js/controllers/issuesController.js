7bec2b6fa2015a99f36432cdbd2af62c33015c89
Stas <stas@ekreative.com>
2017-02-03 18:27:22 +0200

    id

--- js/controllers/issuesController.js
+++ js/controllers/issuesController.js
@@ -1,10 +1,10 @@
 app.controller('issuesCtrl', function ($scope, Api, $stateParams) {
-    Api.get('issues.json/')
+    Api.get('issues.json?project_id=' + $stateParams.id)
         .then(function (data, status) {
             console.log(data);
 
             $scope.issues = data.data.issues;
-            console.log($scope.issues);
+            console.log($scope.issues, $stateParams.id);
         }, function (data, status) {
             console.log(data);
         });
